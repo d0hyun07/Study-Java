9102f77f3ee44c23a424e74899a641bd44b51f95
d0hyun07 <strongdh07@gmail.com>
2023-03-26 22:51:08 +0900

    1st Java

---
+++ src/Ex2/Ex2_6.java
@@ -0,0 +1,17 @@
+package Ex2;
+
+import java.sql.SQLOutput;
+
+public class Ex2_6 {
+    public static void main(String[] args) {
+        int x = 100;
+        double pi = 3.14;
+        char ch = 'a';
+        String str = "abc";
+
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(str);
+    }
+}
