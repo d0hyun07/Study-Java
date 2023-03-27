0b490c791c175dfbca2521edab3fa4a378315d66
d0hyun07 <strongdh07@gmail.com>
2023-03-27 23:26:40 +0900

    2nd Java

--- src/Ex2/Ex2_10.java
+++ src/Ex2/Ex2_10.java
@@ -8,5 +8,9 @@ public class Ex2_10 {
 
         System.out.print("두자리 정수를 하나 입력해주세요.>");
         String input = scanner.nextLine();
+        int num = Integer.parseInt(input);
+
+        System.out.println("입력내용 :"+input);
+        System.out.printf("num=%d%n", num);
     }
 }
