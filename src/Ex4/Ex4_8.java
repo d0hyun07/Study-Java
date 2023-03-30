28a5432a92e6f3c3e6888384865596a94470145d
d0hyun07 <strongdh07@gmail.com>
2023-03-30 23:11:45 +0900

    5th Java

---
+++ src/Ex4/Ex4_8.java
@@ -0,0 +1,9 @@
+package Ex4;
+
+public class Ex4_8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++){ // 괄호{}안의 문잘을 3번 반복
+            System.out.println("Hello");
+        }
+    }
+}
